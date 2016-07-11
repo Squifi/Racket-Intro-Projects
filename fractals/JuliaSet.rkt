159f8d7aa4fb0f2cae6affc7569a92a23e376ab3
squifi <squifi@entwicklerfreu.de>
2016-07-11 21:25:33 +0200

    Added the complex numbers back with a working example and accumulator printing out the values as an example

--- fractals/JuliaSet.rkt
+++ fractals/JuliaSet.rkt
@@ -6,8 +6,6 @@
 (define w (open-viewport "Julia Set" 300 300))
 ; viewport window appears
 
-
-
 ; quick example on how to draw a single Pixel on the screen
 ; the posn struct has an #extra-constructore, therefor (make-posn x y),
 ; is required here.
@@ -16,15 +14,15 @@
 
 (define (mult-list p n c acc)
   [let ([newp (+ [+ (sqr p) n] c)])
-    [(draw-pixel w) (make-posn newp n) "blue"]
+    ;[(draw-pixel w) (make-posn newp n) "blue"] - if i have integers
     (cond
       ([= 8 n] acc)
       ((mult-list newp (+ n 1) c (cons newp acc))))])
 
-
-
+; changed make-posn beforehand gave p = 1+0.5i and c -0.7+2.701565i and
+; just got complete bollocks from the drawing framework.
+(mult-list 1.0+0.5i 0 -0.7+0.27015i '())
 
 ; if required, can also use the x on the top right to close the window
 ;(close-viewport w)
-
 ;(close-graphics)
